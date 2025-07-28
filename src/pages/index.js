2521a40ca082957e98235c5af4a5236d0667a365
Emerson Silva Ricardo <92190123+EmersonRicard0@users.noreply.github.com>
2025-07-28 14:11:34 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -38,17 +38,6 @@ export default function HomePage({ isos }) {
           ))}
         </div>
       </main>
-
-      <footer className="footer">
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
     </div>
   );
 }
