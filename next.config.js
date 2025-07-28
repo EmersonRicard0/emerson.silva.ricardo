c15d13d7354ae4cfa47fc7d06b774d0c7efe9f26
Emerson Silva Ricardo <92190123+EmersonRicard0@users.noreply.github.com>
2025-07-28 12:57:20 -0300

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,8 @@
+// next.config.js
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Nada de especial por enquanto, usará as configurações padrão do Next.js.
+  // Garanta que este arquivo está na raiz do seu projeto e é 'next.config.js' (não .ts)
+};
+
+module.exports = nextConfig;
